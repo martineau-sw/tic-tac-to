39c289b113d84a1b82fbdb0735889022550359c0
Seth Martineau <martineausw@gmail.com>
2024-11-23 17:52:23 -0500

    feat: complete ui, revise win state validation, and refactor

--- script.js
+++ script.js
@@ -12,47 +12,30 @@ const Gameboard = (() => {
     "+---+---+", "--+-+-+--"
   ];
 
-  const moveResponse = {
-    bad: -1,
-    occupied: 0, 
-    good: 1
-  };
-
-  const boardStatus = {
-    continue: 0,
-    xWins: 1,
-    oWins: 2,
-    tie: 3
-  }
-
   const placeMark = (index, mark) => {
-    const markInvalid = mark !== 1 && mark !== 2;
-    const indexOutOfRange = index < 0 || index > 8;
-    const indexOccupied = board[index] > 0;
-
-    if (markInvalid || indexOutOfRange) {
-      console.error('invalid params');
-      return moveResponse.bad;
-    }
-
-    if (indexOccupied) return moveResponse.occupied;
-
+    if (board[index] > 0) return false;
     board[index] = mark;
-    return moveResponse.good;
+    return true;
   };
 
   const resetBoard = () => {
     board.fill(0);
   };
 
-  const getStatus = (isX) => {
-    if (!board.includes(0)) return boardStatus.tie;
-    const playerWin = winStates.includes(toIsomorph(isX));
-    if (playerWin) return isX ? 
-      boardStatus.xWins :
-      boardStatus.oWins ;          
+  const checkWin = (playerX) => {
     
-    return boardStatus.continue;
+    const string = toIsomorph(playerX);
+    if (winStates.includes(string)) 
+      return playerX ? 1 : 2;
+    for (const win of winStates) {
+      let matches = 0;
+      for (let i = 0; i < win.length; i++) {
+        if(win.at(i) === string.at(i) && win.at(i) === '+') matches++;
+      }
+      if(matches === 3) return playerX ? 1 : 2;
+    }
+    if (!board.includes(0)) return 3;
+    return 0;
   };
 
   const toString = () => {
@@ -63,68 +46,150 @@ const Gameboard = (() => {
     `${c[board[6]]} ${c[board[7]]} ${c[board[8]]}\n`
   };
 
-  const toIsomorph = isX => {
-    return board.reduce((string, mark) => {
+  function toIsomorph(isX) {
+    const iso = board.reduce((string, mark) => {
+      let sign = '-';
+      if (isX && mark === 1 || !isX && mark === 2) {
+        sign = '+';
+      } 
 
-      const toSign = () => {
-        return isX ?
-          mark === 1 ? '+' : '-' :
-          mark === 2 ? '+' : '-' ;
-      };
-
-      return string + toSign();
+      return string + sign;
     }, '');
+
+    console.log(iso);
+    return iso;
   };
 
-  return { moveResponse, boardStatus, placeMark, resetBoard, getStatus, toString }
+  return { placeMark, resetBoard, checkWin, toString }
 })();
 
 const GameState = (() => {
 
-  let isX = true;
-  let state = Gameboard.boardStatus.continue;
+  let xName;
+  let oName;
 
-  const isTurnX = () => isX;
-  const getState = () => state;
-  
-  const submit = (index) => {
-    const marker = isX ? 1 : 2;
-    const moveResponse = Gameboard.placeMark(index, marker);
-    state = Gameboard.getStatus(isX);
-    isX = (state === Gameboard.boardStatus.continue) ? !isX : !isX;
-    return moveResponse;
+  const getXName = () => xName;
+  const getOName = () => oName;
+
+  const setXName = name => xName = name;
+  const setOName = name => oName = name;
+
+  const submit = (index, x) => {
+    if(!Gameboard.placeMark(index, x ? 1 : 2)) 
+      return { state: -1, playerX: x };
+    let winner = Gameboard.checkWin(x)
+    if(winner > 0) return { state: winner, playerX: !x };
+    return {
+      state: 0,
+      playerX: !x
+    }
   };
 
-  return { submit, getState, isTurnX };
+  const restart = (frame) => {
+    Gameboard.resetBoard();
+
+    if (frame.state === 3) 
+      frame.state = Math.random() > 0.5;
+
+    return { state: 0, playerX: !frame.state }
+  }
+
+  return { 
+    submit, restart, getXName, getOName, 
+    setXName, setOName
+  };
 })();
 
-const Display = ((document) => {
+const Display = (document => {
 
-  const idToIndex = (id) => {
+  let frame;
+  const x = document.querySelector('.x').cloneNode(true);
+  const o = document.querySelector('.o').cloneNode(true);
+
+  function idToIndex(id) {
     return +(id.slice(4)-1);
   };
 
-  const attachListeners = () => {
-    const x = document.querySelector('#x').cloneNode(true);
-    const o = document.querySelector('#o').cloneNode(true);
-
-    for (let i = 1; i < 10; i++) {
-      const cellElement = document.getElementById(`cell${i}`);
-      cellElement.addEventListener('click', (e) => {
-        if (GameState.getState() !== Gameboard.boardStatus.continue) return;
-        const cell = e.target;
-        const marker = GameState.isTurnX() ? x.cloneNode(true) : o.cloneNode(true);
-        const moveResponse = GameState.submit(idToIndex(cell.id));
-        if (moveResponse !== Gameboard.moveResponse.good) return;
-        cell.appendChild(marker);
-      });
+  function displayResult() {
+    let message;
+    switch(frame.state) {
+      case 2: 
+        message = `${GameState.getXName()} wins!`;
+        break;
+      case 1:
+        message = `${GameState.getOName()} wins!`;
+        break;
+      case 3:
+        message = `It's a tie!`;
+        break;
+      default:
+        message = `Something went wrong :/`;
+        break;
     }
 
-    document.querySelector('#x').remove();
-    document.querySelector('#o').remove();
+    const results = document.querySelector('#results');
+    results.querySelector('h2').textContent = message;
+    results.showModal();
+  }
+
+  function addCellOnClick() {
+    for (let i = 1; i < 10; i++) {
+      const cellElement = document.getElementById(`cell${i}`);
+      cellElement.addEventListener('mousedown', e => {
+        frame = GameState.submit(idToIndex(e.target.closest('.cell').id), frame.playerX);
+        updateMarker(e.target.closest('.cell'));
+      });
+    }
+  }
+
+  function addNamesOnClick() {
+    const x = document.querySelector('#xName');
+    const o = document.querySelector('#oName');
+    names.addEventListener('submit', e => {
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      GameState.setXName(x.value ? x.value : 'X');
+      GameState.setOName(o.value ? o.value : 'O');
+      e.target.closest('dialog').close();
+    });
+  }
+
+  function addRestartOnClick() {
+    const restart = document.querySelector('#restart');
+
+    restart.addEventListener('click', () => {
+      for (const child of document.querySelector('#game').children) 
+        if (child.hasChildNodes()) child.firstChild.remove();
+      
+      GameState.restart(frame);
+      results.close();
+    });
+  }
+
+  function updateMarker(cell) {
+    if (frame.state < 0) return;
+    if (frame.state > 0) displayResult();
+    cell.appendChild(frame.playerX ? 
+      x.cloneNode(true) :
+      o.cloneNode(true));
+  } 
+
+  const attachListeners = () => {
+    addNamesOnClick();
+    addCellOnClick();
+    addRestartOnClick();
   };
 
-  return { attachListeners };
+  const init = () => {
+    frame = { state: 0, playerX: false };
+    attachListeners();
+    document.querySelector('#names').showModal()
+    document.querySelector('.x').remove();
+    document.querySelector('.o').remove();
+  }
+
+  return { init };
 })(document);
 
-Display.attachListeners();
+Display.init();
